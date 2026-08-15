24e3115addcb4b28f76d636238a3e89dbc20fb92
Paul <paulwodi06@gmail.com>
2026-08-15 18:50:00 +0100

    Integrate football API with Flask

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import os
+import requests
 
 app = Flask(__name__)
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
 
 
-@app.route("/")
+def get_player_data(player_name):
+    url = "https://v3.football.api-sports.io/players"
+
+    headers = {
+        "x-apisports-key": api_key
+    }
+
+    params = {
+        "team": 42,
+        "search": player_name
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+
+    return response.json()
+
+
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        player = request.form["player"]
+
+        data = get_player_data(player)
+
+        return render_template("index.html", data=data)
+
     return render_template("index.html")
 
 
