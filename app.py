b3ea9bd821971b033a955047cdd26d0f99925b03
Paul <paulwodi06@gmail.com>
2026-08-17 15:43:19 +0100

    Add player search result selection

--- app.py
+++ app.py
@@ -60,6 +60,26 @@ def home():
         if stats_data["results"] == 0:
             return render_template("index.html", error="No statistics found")
 
+        players = []
+
+        for result in search_data["response"]:
+            player = result["player"]
+
+            players.append({
+                "id": player["id"],
+                "name": player["name"],
+                "firstname": player["firstname"],
+                "lastname": player["lastname"],
+                "age": player["age"],
+                "nationality": player["nationality"]
+            })
+
+        return render_template(
+            "index.html",
+            players=players
+        )
+
+        return render_template("index.html")
         player = stats_data["response"][0]["player"]
         statistics = stats_data["response"][0]["statistics"][0]
 
@@ -80,6 +100,40 @@ def home():
         return render_template("index.html", player=player_data)
 
     return render_template("index.html")
+
+
+@app.route("/player/<int:player_id>")
+def player_profile(player_id):
+
+    stats_data = get_player_stats(player_id)
+
+    if stats_data["results"] == 0:
+        return render_template(
+            "index.html",
+            error="No statistics found"
+        )
+
+    player = stats_data["response"][0]["player"]
+    statistics = stats_data["response"][0]["statistics"][0]
+
+    games = statistics["games"]
+    goals = statistics["goals"]
+
+    player_data = {
+        "name": player["name"],
+        "age": player["age"],
+        "nationality": player["nationality"],
+        "position": games["position"],
+        "appearances": games["appearences"],
+        "minutes": games["minutes"],
+        "goals": goals["total"],
+        "assists": goals["assists"]
+    }
+
+    return render_template(
+        "index.html",
+        player=player_data
+    )
 
 
 if __name__ == "__main__":
